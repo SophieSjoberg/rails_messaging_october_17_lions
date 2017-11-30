aacdda263e4522c8a9e5d09bc340b1654bff62f6
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-30 11:08:52 +0100

    Modify step defs for user login, all tests pass

--- features/step_definitions/user_log_in.rb
+++ features/step_definitions/user_log_in.rb
@@ -10,18 +10,18 @@ Given("I fill in the {string} with {string}") do |field_name, value|
   fill_in field_name, with: value
 end
 
-Given("I fill in the {string} with my {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I fill in the {string} with my {string}") do |field_name, value|
+  fill_in field_name, with: value
 end
 
-Given("I click {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I click {string} button") do |button|
+  click_link_or_button button
 end
 
 Then("I should be on the {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/welcome/index'
 end
 
-Then("I should see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should see {string}") do |text|
+  expect(page).to have_content text
 end
