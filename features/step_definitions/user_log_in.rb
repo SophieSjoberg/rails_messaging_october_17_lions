1bd9d4cdac50421d0aa4814c3085f89081b9f993
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-11-30 10:27:27 +0100

    Add Feature for log in and first step def

---
+++ features/step_definitions/user_log_in.rb
@@ -0,0 +1,31 @@
+Given("I visit the landing page") do
+  visit root_path
+end
+
+When("I fill in my email and password") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("Click the {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in the {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in the {string} with my {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on the {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
