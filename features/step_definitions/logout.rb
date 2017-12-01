01fea9684ba8e0a9fb5d4093b4d19e5c59b8da20
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-01 10:19:17 +0100

    Modify log out feature and step def, all tests pass

--- features/step_definitions/logout.rb
+++ features/step_definitions/logout.rb
@@ -1,17 +1,28 @@
-Given("I am on my account page") do
-  # expect(current_path).to eq root_path
-  visit new_user_session_path
+# Given("I am on my account page") do
+#   # expect(current_path).to eq root_path
+#   visit new_user_session_path
+# end
+#
+# When("I logout") do
+#   # |link|
+#   # click_link_or_button link
+#   logout(:user)
+# end
+#
+#
+# Then("I should redirected to landing page") do
+#   visit destroy_user_session_path
+# end
+#
+# Then("I see {string}") do |string|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+
+Given /^I am logged in as a user$/ do
+  @current_user = User.create!(:username => 'user', :password => 'password')
+  login_as(@current_user, :scope => :user)
 end
 
-When("I click the {string} link") do |link|
-  click_link_or_button link
-end
-
-
-Then("I should redirected to landing page") do
-  visit destroy_user_session_path
-end
-
-Then("I see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given /^I log out$/ do
+  logout
 end
