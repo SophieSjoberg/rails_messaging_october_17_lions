ffc8c01d1ab283e59849216f9747bd27a3310b51
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-01 14:06:21 +0100

    Delete duplicated step defs

--- features/step_definitions/logout.rb
+++ features/step_definitions/logout.rb
@@ -1,8 +1,1 @@
-Given("I am logged in as {string}") do |email|
-  user = User.find_by(email: email)
-  login_as(user, scope: :user)
-end
 
-Given("I visit the landing page") do
-  visit root_path
-end
